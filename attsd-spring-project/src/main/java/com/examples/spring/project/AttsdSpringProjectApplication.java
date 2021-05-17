8731155e902fcc6348b72f766d7483ef9464f1a0
LorenzoMorg <lorenzomorganti94@gmail.com>
2021-05-17 22:41:28 +0200

    some initial files

---
+++ attsd-spring-project/src/main/java/com/examples/spring/project/AttsdSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.examples.spring.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttsdSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttsdSpringProjectApplication.class, args);
+	}
+
+}
---
+++ attsd-spring-project/src/test/java/com/examples/spring/project/AttsdSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.spring.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttsdSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
