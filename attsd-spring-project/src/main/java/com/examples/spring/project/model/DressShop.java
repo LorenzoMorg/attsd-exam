90921dcfe42923010c5cc4f872fb40c44525d5c5
LorenzoMorg <lorenzomorganti94@gmail.com>
2021-05-18 17:43:22 +0200

    DressShop updated

--- attsd-spring-project/src/main/java/com/examples/spring/project/model/DressShop.java
+++ attsd-spring-project/src/main/java/com/examples/spring/project/model/DressShop.java
@@ -1,22 +1,30 @@
 package com.examples.spring.project.model;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class DressShop {
 	
-	private long id;
+	@Id @GeneratedValue
+	private Long id;
 	private String name;
 	private int averagePrice;
+	
+	public DressShop() {
+	}
 
-	public DressShop(long id, String name, int averagePrice) {
+	public DressShop(Long id, String name, int averagePrice) {
 		this.id = id;
 		this.name = name;
 		this.averagePrice = averagePrice;
 	}
-	
-	public DressShop() {	
-	}
 
-	public long getId() {
-		return id;
+	public Long getId() {
+			return id;
 	}
 
 	public String getName() {
@@ -30,6 +38,49 @@ public class DressShop {
 	@Override
 	public String toString() {
 		return "DressShop [id=" + id + ", name=" + name + ", averagePrice=" + averagePrice + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, averagePrice);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DressShop other = (DressShop) obj;
+		if (averagePrice != other.averagePrice)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAveragePrice(int avgPrice) {
+		this.averagePrice = avgPrice;		
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 }
