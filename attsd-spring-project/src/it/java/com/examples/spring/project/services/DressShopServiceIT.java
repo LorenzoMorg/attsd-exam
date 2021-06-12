0bbcfdbe335d5be2c671f97f5186a77bf3ecc9d8
LorenzoMorg <lorenzomorganti94@gmail.com>
2021-06-12 19:00:41 +0200

    attribute target price from int to Long

--- attsd-spring-project/src/it/java/com/examples/spring/project/services/DressShopServiceIT.java
+++ attsd-spring-project/src/it/java/com/examples/spring/project/services/DressShopServiceIT.java
@@ -7,8 +7,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.examples.spring.project.model.DressShop;
@@ -16,7 +17,8 @@ import com.examples.spring.project.repositories.DressShopRepository;
 import cucumber.api.java.After;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ActiveProfiles("mysql")
+@DataJpaTest
 @Import(DressShopService.class)
 public class DressShopServiceIT {
 
@@ -38,21 +40,21 @@ public class DressShopServiceIT {
 
 	@Test
 	public void test_serviceInsertIntoRepository() {
-		DressShop ds = service.saveIntoDb(new DressShop(Long.valueOf(1), "Lacoste", 18));
+		DressShop ds = service.saveIntoDb(new DressShop(null, "Lacoste", 18L));
 		assertThat(repository.findById(ds.getId())).isPresent();
 	}
 	
 	@Test
 	public void test_serviceUpdateIntoRepository() {
-		DressShop ds = repository.save(new DressShop(Long.valueOf(1), "Lacoste", 18));
-		DressShop ds2 = service.saveIntoDb(new DressShop(ds.getId(), "Colmar", 30));
+		DressShop ds = repository.save(new DressShop(null, "Lacoste", 18L));
+		DressShop ds2 = service.saveIntoDb(new DressShop(ds.getId(), "Colmar", 30L));
 		
 		assertThat(repository.findById(ds.getId())).contains(ds2);
 	}
 	
 	@Test
 	public void delete_dressShop() {
-		DressShop ds = service.saveIntoDb(new DressShop(Long.valueOf(1), "Lacoste", 18));
+		DressShop ds = service.saveIntoDb(new DressShop(null, "Lacoste", 18L));
 		assertEquals(1, repository.count());
 		service.delete(ds);
 		assertEquals(0, repository.count());
